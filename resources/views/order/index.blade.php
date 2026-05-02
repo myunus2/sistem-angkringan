ebe08f5ca7756744c213a542e001a4754aa70af0
anis180704 <fauziahannisatun790@gmail.com>
2026-05-02 20:59:58 +0700

    sjsjjs

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-<<<<<<< HEAD
-    <title>Menu Angkringan Modernn</title>
-=======
     <title>Menu Angkringan Cakra</title>
->>>>>>> 160d3f1500f08896dfaf342b25399801095fa66e
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
     <style>
