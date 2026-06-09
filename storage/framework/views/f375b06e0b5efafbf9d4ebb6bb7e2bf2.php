026c210bc50942f9ef574449891e642c10cc6087
m yunus <april042305@gmail.com>
2026-06-09 23:27:04 +0700

    update lockfile untuk PHP 8.3

--- storage/framework/views/f375b06e0b5efafbf9d4ebb6bb7e2bf2.php
+++
@@ -1,3 +0,0 @@
-<svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-  <path fill-rule="evenodd" d="M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z" clip-rule="evenodd"/>
-</svg><?php /**PATH C:\xampp1\htdocs\sistem-angkringan\storage\framework\views/3c13c39c1faa57bcf1200cce41083862.blade.php ENDPATH**/ ?>
