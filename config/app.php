ef41101ae9161d739fe4cd4bf4c06fecec18d0d4
m yunus <april042305@gmail.com>
2026-06-11 06:22:22 +0700

    fix: hardcode APP_URL menjadi HTTPS production

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => 'https://sistem-angkringan-production.up.railway.app',
 
     /*
     |--------------------------------------------------------------------------
